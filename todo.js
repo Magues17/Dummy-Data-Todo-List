a5fb8dbb49492ccc6da6e95e89d2c7d47a79d96d
Magues17 <mikel.d.campbell@gmail.com>
2023-03-01 20:32:42 -0600

    filternotworking undefined

--- todo.js
+++ todo.js
@@ -40,3 +40,51 @@ ol.appendChild(li);
     }
 
 
+    const filterTodosOldSchool = () => {
+
+        let filtered = [];
+        let userId = 2
+        for (let index = 0; index < arrayOfTodos.length; index++) {
+            const todo = arrayOfTodos[index];
+            
+            if (todo.userId === userId) {
+                filtered.push(todo)
+            }
+        }
+        console.log('filtered:', filtered)
+    
+        const filteredTodosElement = document.getElementById("todo-list");
+
+        filteredTodosElement.innerHTML = "";
+        
+            // const ol = document.getElementById("todo-list");
+            filtered.forEach((todo) => {
+                const listItemElement = document.createElement("li");
+
+                    const textNode = document.createTextNode(todo.task);
+
+                         listItemElement.appendChild(textNode);
+
+                             filteredTodosElement.appendChild(listItemElement);
+            });
+                
+     }
+                
+  
+
+    const filterTodos = () => {
+        // return arrayOfTodos.length
+        let filteredByUserId = arrayOfTodos.filter((todo) => {
+            let userId = 2
+            //return todo
+            if (todo.userId === userId && todo.completed) {
+                return todo
+            }
+        
+        })
+        console.log('filteredByUserId:', filteredByUserId)
+    }
+
+
+
+
